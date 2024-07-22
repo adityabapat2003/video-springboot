5ed595888d2e14b4f26ba1597ac18cc4c602bba8
Aditya Bapat <97380452+adityabapat2003@users.noreply.github.com>
2024-07-22 14:16:38 +0530

    first commit

---
+++ src/main/java/com/aditya/videocall/VideocallApplication.java
@@ -0,0 +1,42 @@
+package com.aditya.videocall;
+
+import com.aditya.videocall.user.User;
+import com.aditya.videocall.user.UserService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class VideocallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideocallApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			UserService service
+	) {
+		return args -> {
+			service.register(User.builder()
+					.username("Adi")
+					.email("adi@mail.com")
+					.password("aaa")
+					.build());
+
+			service.register(User.builder()
+					.username("John")
+					.email("john@mail.com")
+					.password("aaa")
+					.build());
+
+			service.register(User.builder()
+					.username("Anny")
+					.email("anna@mail.com")
+					.password("aaa")
+					.build());
+		};
+	}
+
+}
